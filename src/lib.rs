e0954a5d05bffc1f5b93c6465013a2a92d2611e7
p-w-g <p.grzelczyk@gmail.com>
2023-05-07 12:00:02 +0200

    add basic test for collatz fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
+mod collatz;
 #[cfg(test)]
 mod tests {
     // use std::result;
+
+    use super::*;
 
     #[test]
     fn sanity_test() {
         let result: i8 = 2 + 2;
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn collatz_tests() {
+        let case_1 = collatz::collatz(1);
+        assert_eq!(case_1, 0);
     }
 }
