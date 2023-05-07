24517c162e3edf0e9dea81f09c6d539bf2c389a9
p-w-g <p.grzelczyk@gmail.com>
2023-05-07 12:21:48 +0200

    fix broken logic, add missing step counter

--- src/collatz.rs
+++ src/collatz.rs
@@ -1,9 +1,9 @@
-pub fn collatz(input: i128) -> i32 {
+pub fn collatz(input: i128) -> i128 {
     if input == 1 {
         0
     } else if input % 2 == 0 {
-        collatz(input / 2)
+        collatz(input / 2) + 1
     } else {
-        collatz(3 * input + 1)
+        collatz(3 * input + 1) + 1
     }
 }
